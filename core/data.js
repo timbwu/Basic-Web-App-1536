dfa0506ee819f1be69c5a4b31283a674098994df
Tim Wu <timwu767@gmail.com>
2019-04-02 14:10:23 -0700

    deleted images and added recipes

--- core/data.js
+++ core/data.js
@@ -1,15 +1,15 @@
-let recipeImg1 = "<img src=\"./images/r1.jpg\" class=\"img-responsive\" style=\"width:100%\" alt=\"Image\">";
+let recipeImg1 = "<img src=\"./images/r1.png\" class=\"img-responsive\" style=\"width:100%\" alt=\"Image\">";
 let recipeImg2 = "<img src=\"./images/r2.jpeg\" class=\"img-responsive\" style=\"width:100%\" alt=\"Image\">";
 let recipeImg3 = "<img src=\"./images/r3.jpg\" class=\"img-responsive\" style=\"width:100%\" alt=\"Image\">";
-let recipeImg4 = "<img src=\"./images/r3.jpg\" class=\"img-responsive\" style=\"width:100%\" alt=\"Image\">";
-let recipeImg5 = "<img src=\"./images/r3.jpg\" class=\"img-responsive\" style=\"width:100%\" alt=\"Image\">";
-let recipeImg6 = "<img src=\"./images/r3.jpg\" class=\"img-responsive\" style=\"width:100%\" alt=\"Image\">";
-let recipeImg7 = "<img src=\"./images/r3.jpg\" class=\"img-responsive\" style=\"width:100%\" alt=\"Image\">";
-let recipeImg8 = "<img src=\"./images/r3.jpg\" class=\"img-responsive\" style=\"width:100%\" alt=\"Image\">";
-let recipeImg9 = "<img src=\"./images/r3.jpg\" class=\"img-responsive\" style=\"width:100%\" alt=\"Image\">";
-let recipeImg10 = "<img src=\"./images/r3.jpg\" class=\"img-responsive\" style=\"width:100%\" alt=\"Image\">";
+let recipeImg4 = "<img src=\"./images/r4.jpg\" class=\"img-responsive\" style=\"width:100%\" alt=\"Image\">";
+let recipeImg5 = "<img src=\"./images/r5.jpg\" class=\"img-responsive\" style=\"width:100%\" alt=\"Image\">";
+let recipeImg6 = "<img src=\"./images/r6.jpg\" class=\"img-responsive\" style=\"width:100%\" alt=\"Image\">";
+let recipeImg7 = "<img src=\"./images/r7.jpg\" class=\"img-responsive\" style=\"width:100%\" alt=\"Image\">";
+let recipeImg8 = "<img src=\"./images/r8.jpg\" class=\"img-responsive\" style=\"width:100%\" alt=\"Image\">";
+let recipeImg9 = "<img src=\"./images/r9.jpg\" class=\"img-responsive\" style=\"width:100%\" alt=\"Image\">";
+let recipeImg10 = "<img src=\"./images/10.jpg\" class=\"img-responsive\" style=\"width:100%\" alt=\"Image\">";
 
-let recipeNames = ["Three Cheese Blend", "Sauteed Green Beans", "5 Minute Spaghetti"];
+let recipeNames = ["Three Cheese Blend", "Sauteed Green Beans", "5 Minute Spaghetti", "Philly Cheesesteak", "Mini Shepherd's Pie", "Homemade Coffee Creamer", "Queso & Chicken Nachos", "Siriracha Turkey Meatballs", "Grilled Salmon Kabobs", "Hot Dog", "Mozzarella Sticks"];
 
 module.exports = {
   getHTML: function () {
